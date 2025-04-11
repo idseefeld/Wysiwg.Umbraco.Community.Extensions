9a95521b44a3806dcec68c51e803c0d32dc3d37c
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2025-04-11 19:22:45 +0200

    update uSync, add manual pack command file

---
+++ src/wysiwgUmbracoCommunityExtensions/manually-pack.cmd
@@ -0,0 +1,3 @@
+rem This script is used to build the Umbraco Community Extensions project and create a NuGet package. 
+dotnet pack --configuration Release /p:Version=15.3.0-rc1 --output z:\nuget.local
+pause
