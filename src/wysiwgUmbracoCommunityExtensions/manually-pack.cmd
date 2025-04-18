1d93ba4f395fa90628306ff756752a25973e4628
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2025-04-18 13:04:51 +0200

    prepare release 15.3.1

--- src/wysiwgUmbracoCommunityExtensions/manually-pack.cmd
+++ src/wysiwgUmbracoCommunityExtensions/manually-pack.cmd
@@ -1,3 +1,3 @@
 rem This script is used to build the Umbraco Community Extensions project and create a NuGet package. 
-dotnet pack --configuration Release /p:Version=15.3.0 --output z:\nuget.local
+dotnet pack --configuration Release /p:Version=15.3.1 --output z:\nuget.local
 pause
