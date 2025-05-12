ebfc94285048a7a30445cfbd86145c0bd1b38b15
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2025-05-12 21:15:09 +0200

    add missing js files

---
+++ src/WysiwgUmbracoCommunityExtensions/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/index-ClocuwdO.js
@@ -0,0 +1,6 @@
+import { W as m, a as o } from "./wysiwg-cropped-image.element-V1msS2hc.js";
+export {
+  m as WysiwgBlocksImageCropElement,
+  o as WysiwgCroppedImageElement
+};
+//# sourceMappingURL=index-ClocuwdO.js.map
