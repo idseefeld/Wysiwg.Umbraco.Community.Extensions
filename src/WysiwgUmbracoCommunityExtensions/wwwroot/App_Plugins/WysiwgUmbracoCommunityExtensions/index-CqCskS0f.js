523d8d12ef5258f77802f9e75f456a42f83467f5
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2025-05-15 11:39:04 +0200

    fix issue 66

---
+++ src/WysiwgUmbracoCommunityExtensions/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/index-CqCskS0f.js
@@ -0,0 +1,6 @@
+import { W as m, a as o } from "./wysiwg-cropped-image.element-Cb-m-nWn.js";
+export {
+  m as WysiwgBlocksImageCropElement,
+  o as WysiwgCroppedImageElement
+};
+//# sourceMappingURL=index-CqCskS0f.js.map
