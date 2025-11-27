8a9c9e9bbce6ce3a8eac9aee39eae37f4d66345e
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2025-11-27 13:35:56 +0100

    fix openApi reference

--- src/wysiwgUmbracoCommunityExtensions/Composers/wysiwgUmbracoCommunityExtensionsApiComposer.cs
+++ src/wysiwgUmbracoCommunityExtensions/Composers/wysiwgUmbracoCommunityExtensionsApiComposer.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Api.Management.OpenApi;
 using Umbraco.Cms.Api.Common.OpenApi;
 using WysiwgUmbracoCommunityExtensions.Services;
+using Microsoft.OpenApi;
 
 
 namespace WysiwgUmbracoCommunityExtensions.Composers
