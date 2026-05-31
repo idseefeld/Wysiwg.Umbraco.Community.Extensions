cc5c05bfe54d5c088b16bdbd49043afbf72d1dfc
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2026-05-31 13:44:18 +0200

    bump version 17.0.6

--- src/WysiwgUmbracoCommunityExtensionsUmb17/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/dashboard.element-B4t0hETL.js
+++ src/WysiwgUmbracoCommunityExtensionsUmb17/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/dashboard.element-B4t0hETL.js
@@ -46,7 +46,7 @@ class C {
       return t;
   }
 }
-const x = "17.0.5", S = {
+const x = "17.0.6", S = {
   version: x
 };
 var z = Object.defineProperty, U = Object.getOwnPropertyDescriptor, a = (e, t, s, o) => {
@@ -336,4 +336,4 @@ export {
   r as WysiwgDashboardElement,
   O as default
 };
-//# sourceMappingURL=dashboard.element-KEfqMBul.js.map
+//# sourceMappingURL=dashboard.element-B4t0hETL.js.map
--- src/WysiwgUmbracoCommunityExtensionsUmb17/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/wysiwg-umbraco-community-extensions.js
+++ src/WysiwgUmbracoCommunityExtensionsUmb17/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/wysiwg-umbraco-community-extensions.js
@@ -11,7 +11,7 @@ const a = [
     type: "sectionView",
     alias: "WysiwgDashboardElement.Section",
     name: "WYSIWYG Extensions Section",
-    element: () => import("./dashboard.element-KEfqMBul.js"),
+    element: () => import("./dashboard.element-B4t0hETL.js"),
     meta: {
       label: "WYSIWYG",
       icon: "icon-settings",
