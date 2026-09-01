8fc0eb7a7a1be0d33b0e242f4ec6df69189b462b
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2026-09-01 18:33:58 +0200

    prepare 18.1.1-rc1

--- src/WysiwgUmbracoCommunityExtensionsUmb17/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/wysiwg-umbraco-community-extensions.js
+++ src/WysiwgUmbracoCommunityExtensionsUmb17/wwwroot/App_Plugins/WysiwgUmbracoCommunityExtensions/wysiwg-umbraco-community-extensions.js
@@ -11,7 +11,7 @@ const r = [
     type: "sectionView",
     alias: "WysiwgDashboardElement.Section",
     name: "WYSIWYG Extensions Section",
-    element: () => import("./dashboard.element-d4sn3_P8.js"),
+    element: () => import("./dashboard.element-gjEvEskS.js"),
     meta: {
       label: "WYSIWYG",
       icon: "icon-settings",
